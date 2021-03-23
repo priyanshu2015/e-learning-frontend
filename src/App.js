921b88c3bd27659fe946f5c1d55ac9b2b4068c8f
Adityansh Sharma <sharmaadityansh@gmail.com>
2021-03-24 00:50:51 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import Login from './components/Login'
+import Signup from './components/Signup'
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React hello
-        </a>
-      </header>
+    <div>
+    <div className="container">
+      <Signup />  
+    </div>
+    <div className="container">
+      <Login />  
+    </div>
     </div>
   );
 }
