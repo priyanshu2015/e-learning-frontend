edd4b67839a78f72038748a31284d3f5c1a6f714
Priyanshu Gupta <priyanshu.gupta2008@gmail.com>
2021-03-24 09:36:14 +0530

    integrating login and signup with backend

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState} from 'react'
 
 const Login = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    //const history = useHistory();
+    // useEffect(() => {
+    //     if (localStorage.getItem('user-info')){
+    //         history.pushState("/add")
+    //     }
+    // }, []);
+    async function login(){
+        console.warn(email, password);
+        let item={email, password};
+        let result = await fetch("http://localhost/e-learning-platform/api/login.php",{
+            method:'POST',
+            mode: 'cors',
+            headers:{
+                "Content-Type":"application/json",
+                "Accept":'application/json'
+            },
+            body: JSON.stringify(item),
+        });
+        result = await result.json();
+        localStorage.setItem("user-info", JSON.stringify(result))
+        //history.push("/add");
+
+    }
     return (
         <>
             <h1>Welcome Back</h1>
@@ -8,15 +33,15 @@ const Login = () => {
                 
                 <div>
                     <label htmlFor="email">Email</label>
-                    <input type="text" name="email" id="email" />
+                    <input type="text" name="email" onChange={(e)=>setEmail(e.target.value)} id="email" />
                 </div>
 
                 <div>
                     <label htmlFor="password">Password</label>
-                    <input type="text" name="password" id="password" />
+                    <input type="password" name="password" onChange={(e)=>setPassword(e.target.value)} id="password" />
                 </div>
 
-                <button className="btn" type="submit">Log In</button>
+                <button onClick={login} className="btn" type="button">Log In</button>
             </form>
         </>
     )
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,33 +1,60 @@
-import React from 'react'
+import React, { useEffect, useState} from 'react'
 
 const Signup = () => {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [phone, setPhone] = useState("");
+    const [password, setPassword] = useState("");
+    //const history = useHistory();
+    // useEffect(() => {
+    //     if (localStorage.getItem('user-info')){
+    //         history.pushState("/add")
+    //     }
+    // }, []);
+    async function signup(){
+        console.warn(name, email, phone, password);
+        let item={name, email, phone, password};
+        let result = await fetch("http://localhost/e-learning-platform/api/signup.php",{
+            method:'POST',
+            mode: 'cors',
+            headers:{
+                "Content-Type":"application/json",
+                "Accept":'application/json'
+            },
+            body: JSON.stringify(item),
+        });
+        result = await result.json();
+        alert(result["message"])
+        //history.push("/add");
+
+    }
     return (
         <>
             <h1>Lets Get Started</h1>
               <form action="" className="form-control">
                 <div>
-                    <label htmlFor="username">Fullname</label>
-                    <input type="text" name="username" id="username" />
+                    <label htmlFor="username">Name</label>
+                    <input type="text" name="name" onChange={(e)=>setName(e.target.value)} id="name" />
                 </div>
 
                 
                 <div>
                     <label htmlFor="email">Email</label>
-                    <input type="text" name="email" id="email" />
+                    <input type="text" name="email" onChange={(e)=>setEmail(e.target.value)} id="email" />
                 </div>
 
                 
                 <div>
                     <label htmlFor="phone">Phone Number</label>
-                    <input type="number" name="phone" id="phone" />
+                    <input type="number" name="phone" onChange={(e)=>setPhone(e.target.value)} id="phone" />
                 </div>
 
                 <div>
                     <label htmlFor="password">Password</label>
-                    <input type="text" name="password" id="password" />
+                    <input type="password" name="password" onChange={(e)=>setPassword(e.target.value)} id="password" />
                 </div>
 
-                <button className="btn" type="submit">Register</button>
+                <button onClick={signup} className="btn" type="button">Register</button>
             </form>
         </>
     )
